9d9427e670ef7238b24c8b9f0513d515c0825fdb
Bhekani Khumalo <bhekanik@gmail.com>
2022-12-16 12:40:19 +0000

    Add authentication

--- packages/utils/dist/stringCompare.d.ts
+++
@@ -1,7 +0,0 @@
-interface StringCompareOptions {
-    caseSensitive?: boolean;
-    ignoreWhitespace?: boolean;
-    specialChars?: string[];
-}
-export declare const stringCompare: (stringOne: string, stringTwo: string, options?: StringCompareOptions) => boolean;
-export {};
