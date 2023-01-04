56cc43430f0ec2992254e24aafb548c40bcf4f30
Bhekani Khumalo <bhekanik@gmail.com>
2023-01-04 02:31:02 +0000

    Make header responsive

--- apps/just-working-out/src/lib/components/Header.svelte
+++ apps/just-working-out/src/lib/components/Header.svelte
@@ -3,15 +3,17 @@
 	import NavItem from './NavItem.svelte';
 </script>
 
-<header class="flex gap-4 w-full justify-between items-center px-8">
-	<h1 class="text-md md:text-2xl font-light text-custom-green">
+<header
+	class="flex flex-col md:flex-row pt-8 md:pt-0 gap-4 w-full justify-between items-center px-8"
+>
+	<h1 class="text-2xl font-light text-custom-blue">
 		<a href="/">Just Working Out</a>
 	</h1>
 
 	<nav>
 		<ul class="flex gap-2 md:gap-4">
-			<NavItem href="/schedule">Schedule</NavItem>
 			<NavItem href={`/workout/${format(new Date(), 'EEEE')}`}>Workout</NavItem>
+			<NavItem href="/schedule">Schedule</NavItem>
 		</ul>
 	</nav>
 </header>
--- apps/just-working-out/src/lib/components/NavItem.svelte
+++ apps/just-working-out/src/lib/components/NavItem.svelte
@@ -2,6 +2,6 @@
 	export let href: string;
 </script>
 
-<li class="text-custom-green hover:text-custom-red h-full p-4 px-1 md:px-4">
+<li class="text-custom-green hover:text-custom-red h-full py-0 md:py-4 px-1 md:px-4">
 	<a {href}><slot /></a>
 </li>
